3d560a31c77026fc795ebf5f6251e7594ae7b4bf
SherryMagic <sherryliang6266@gmail.com>
2026-01-20 15:16:04 -0700

    Added page.js and StudentInfo.js in the file week-2. Edited page.js in app.

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,11 @@
+export default function StudentInfo() {
+    return (
+        <section>
+            <h2>Sherry Liang</h2>
+            <p>GitHub: 
+                <a href="https://github.com/SherryMagic/cprg306-assignments" style={{ textDecoration: "underline"}}target ="_blank">SherryMagic/cprg306-assignments
+                </a>
+            </p>
+        </section>
+    );
+}
