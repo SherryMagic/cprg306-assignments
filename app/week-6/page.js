17edd723402be03a83619f00965e18e4a949ffa6
SherryMagic <sherryliang6266@gmail.com>
2026-02-22 22:26:12 -0700

    edited text color of shopping list title to pink, and added dark mode support to the page and item components.

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -24,7 +24,7 @@ export default function Week6Page() {
     // Pass your items state variable to the items prop.
     return (
         <main className="min-h-screen bg-gray-50 dark:bg-gray-900 dark:text-pink p-8">
-            <h1 className="text-4xl font-bold text-green-500 mb-6">
+            <h1 className="text-4xl font-bold text-pink-500 mb-6">
                 Shopping List
             </h1>
 
